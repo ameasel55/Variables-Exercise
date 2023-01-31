87fb4c55fe187a794986e5f4f952becd9ba7728c
Adam Measel <ameasel55@gmail.com>
2023-01-31 09:29:21 -0600

    Padma

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number;
+            number = 500;
+
+            int anotherNumber = 3500;
+
+            //celebrityChef = "Padma Lakshmi"
+            //Console.WriteLine(celebrityChef)
+
+
+            string celebrityChefs = "Padma Lakshmi";
+            char someCharacter = '$';
+            bool canCook = true;
+            int firstNumber = 5;
+            double secondNumber = 22.4;
+            decimal thirdNumber = 503.1m;
+
+            Console.WriteLine($"My favorite celebrity chef is {celebrityChefs}. She cooks {firstNumber} meals a day");
+            Console.WriteLine($"She jogs {secondNumber} miles to burn off all of those calories");
+            Console.WriteLine($"Her favorite dish to cook is Hariyali Chicken, and if you lined all those dishes on the ground it would stretch {thirdNumber} miles");
+            Console.WriteLine($"It's crazy but {canCook}");
+            Console.WriteLine($"She charges {someCharacter}{number} for a cameo and {someCharacter}{anotherNumber} for a public appearence");
+
         }
     }
 }
